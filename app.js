e9d9147076458ece49c47db7126a9c3ac1fcff2b
Rajan Walia <rajanwalia4@gmail.com>
2022-09-12 23:17:38 +0530

    Setting up folder structure

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 
 
+// parse the json request body
+app.use(express.json());
+// parse the urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
 
 module.exports = app;
