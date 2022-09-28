acd6a0158d329d433387eef2a166b4e997bf5305
Rajan Walia <rajanwalia4@gmail.com>
2022-09-28 21:30:30 +0530

    user-auth : Added / endpoint

--- src/app.js
+++ src/app.js
@@ -15,9 +15,10 @@ if (config.env !== 'test') {
 app.use(express.json());
 // parse the urlencoded request body
 app.use(express.urlencoded({ extended: true }));
-app.get("/hello", (req, res) => {
+
+app.get("/", (req, res) => {
   return res.send(
-    "Hello World"
+    "Hello, Programmer 👋"
   )
 })
 app.use("/api", require("./routes"));
