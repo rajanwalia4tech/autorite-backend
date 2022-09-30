1ebc7326d0743052c9a95167fbadbfe7d22b922e
Rajan Walia <rajanwalia4@gmail.com>
2022-09-30 17:46:10 +0530

    user-auth : Added the cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const config = require("./config");
 const morgan = require("./config/morgan");
 const httpStatus = require("http-status");
@@ -15,12 +16,27 @@ if (config.env !== 'test') {
 app.use(express.json());
 // parse the urlencoded request body
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.get("/", (req, res) => {
   return res.send(
     "Hello, Programmer 👋"
   )
 })
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, app_version, device_type, access_token,app_secret_key, auth_token, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, DELETE, OPTIONS,PATCH"
+  );
+  res.setHeader("X-Frame-Options", "DENY");
+  next();
+});
+
 app.use("/api", require("./routes"));
 
 // send back a 404 error for any unknown api request
